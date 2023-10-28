99805fcf8bded43e32d4023604e3b4e032092871
HoangNguyet <nguyethoanghan@gmail.com>
2023-10-28 11:12:57 +0700

    BTLML

--- BTLML_ID3&PCA.py
+++ BTLML_ID3&PCA.py
@@ -31,14 +31,13 @@ max_id3 = 0
 for i in range(1,9): #duyệt qua từ 1-8 (i là số components PCA), 
     pca = PCA(n_components = i) #tạo mô hình pca với số thành phần chính được thiết lập là i
     pca.fit(scaler_x) #Huấn luyện mô hình với tập dữ liệu X
-    X_bar = pca.transform(scaler_x) #ánh xạ cho tập dữ liệu gốc X thành X_bar với số chiều đã giảm
-    X_train, X_test, Y_train, Y_test = train_test_split(X_bar, Y, test_size=0.2, shuffle= False)
+    X_bar = pca.transform(scaler_x) # Giảm chiều dữ liệu
+    X_train, X_test, Y_train, Y_test = train_test_split(X_bar, Y, test_size=0.3,shuffle=False)
     #với mỗi i ta xây dựng 1 cây quyết định
     #id3
     id3 = DecisionTreeClassifier(criterion='entropy')
     id3.fit(X_train, Y_train) # huấn luyện cây 
     y_pred_id3 = id3.predict(X_test) #Tính kết quả y_test dựa vào x_test dựa vào mô hình vừa huấn luyện
-    #rate_id3 = metrics.accuracy_score(Y_test, y_pred_id3) #Tính tỉ lệ dự đoán đúng
     if(RateRating(y_pred_id3) > max_id3):
         num_pca_id3 = i #Lưu thuộc tính tốt nhất
         pca_best_id3 = pca #lưu mô hình pca tốt nhất
@@ -50,7 +49,7 @@ form = Tk()
 form.title("Dự đoán bệnh tiểu đường:") 
 form.geometry("750x500")
 
-lable_NhietDo = Label(form, text = "Dự đoán bệnh tiểu đường", font=("Arial", 20), fg = "brown").grid(row = 0, column = 1, pady = 10, sticky="e")
+lable_dudoan = Label(form, text = "Dự đoán bệnh tiểu đường", font=("Arial", 20), fg = "brown").grid(row = 0, column = 1, pady = 10, sticky="e")
 
 group1 = LabelFrame(form, text="Nhập thông tin để dự đoán")
 group1.grid(row=1, column=1, padx=50, pady=30)
@@ -79,7 +78,7 @@ lable_insu = Label(group1, text = "Insulin:").grid(row = 5, column = 1, pady = 1
 textbox_insu = Entry(group1)
 textbox_insu.grid(row = 5, column = 2)
 
-lable_ChatBeo = Label(group1, text = "BMI:").grid(row = 6, column = 1, pady = 10,sticky="e")
+lable_bmi = Label(group1, text = "BMI:").grid(row = 6, column = 1, pady = 10,sticky="e")
 textbox_bmi = Entry(group1)
 textbox_bmi.grid(row = 6, column = 2)
 
@@ -117,8 +116,8 @@ def dudoanID3():
         messagebox.showinfo("Thông báo", "Bạn cần nhập đầy đủ thông tin!")
     else:
         X_dudoan = np.array([Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction, Age]).reshape(1, -1)
-        X_dudoan_bar = pca_best_id3.transform(X_dudoan)
-        y_kqua = modelmax_id3.predict(X_dudoan_bar)
+        X_dudoan_bar = pca_best_id3.transform(X_dudoan) # Dùng mô hình pca tốt nhất để làm giảm chiều dữ liệu
+        y_kqua = modelmax_id3.predict(X_dudoan_bar) # Dùng mô hình id3 tốt nhất để dự đoán kết quả đầu vào
         lb_pred_id3.configure(text= y_kqua[0])
 
 button_1 = Button(group2, text = 'Kết quả dự đoán', font=("Arial Bold", 9), fg = "black", bg = "green", command = dudoanID3)
